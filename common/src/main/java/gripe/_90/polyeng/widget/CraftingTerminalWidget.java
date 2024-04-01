e080285454cf60808f4b6208a4c9ebe636473f4f
90 <hi@90.gripe>
2024-04-01 15:02:45 +0100

    Re-adjust widget position on menu resize operations

--- common/src/main/java/gripe/_90/polyeng/widget/CraftingTerminalWidget.java
+++ common/src/main/java/gripe/_90/polyeng/widget/CraftingTerminalWidget.java
@@ -1,15 +1,22 @@
 package gripe._90.polyeng.widget;
 
 import appeng.client.gui.me.items.CraftingTermScreen;
+import appeng.menu.slot.CraftingTermSlot;
+import com.illusivesoulworks.polymorph.api.client.base.ITickingRecipesWidget;
 import com.illusivesoulworks.polymorph.client.recipe.widget.PlayerRecipesWidget;
 import gripe._90.polyeng.PolymorphicEnergistics;
 import gripe._90.polyeng.mixin.AEBaseMenuAccessor;
+import gripe._90.polyeng.mixin.AbstractContainerScreenAccessor;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.inventory.Slot;
 
-public class CraftingTerminalWidget extends PlayerRecipesWidget {
+public class CraftingTerminalWidget extends PlayerRecipesWidget implements ITickingRecipesWidget {
+    private Slot outputSlot;
+    private int menuHeight;
+
     public CraftingTerminalWidget(CraftingTermScreen<?> screen, Slot outputSlot) {
         super(screen, outputSlot);
+        this.outputSlot = outputSlot;
     }
 
     @Override
@@ -18,5 +25,27 @@ public class CraftingTerminalWidget extends PlayerRecipesWidget {
         var menu = ((CraftingTermScreen<?>) containerScreen).getMenu();
         menu.getPlayer().getLevel().getRecipeManager().byKey(id).ifPresent(recipe -> ((AEBaseMenuAccessor) menu)
                 .invokeSendClientAction(PolymorphicEnergistics.ACTION));
+    }
+
+    @Override
+    public Slot getOutputSlot() {
+        return outputSlot;
+    }
+
+    @Override
+    public void tick() {
+        var screen = (AbstractContainerScreenAccessor) containerScreen;
+
+        if (screen.getImageHeight() != menuHeight) {
+            for (var slot : containerScreen.getMenu().slots) {
+                if (slot instanceof CraftingTermSlot craftingSlot) {
+                    outputSlot = craftingSlot;
+                    resetWidgetOffsets();
+                    break;
+                }
+            }
+
+            menuHeight = screen.getImageHeight();
+        }
     }
 }
--- common/src/main/java/gripe/_90/polyeng/widget/PatternTerminalWidget.java
+++ common/src/main/java/gripe/_90/polyeng/widget/PatternTerminalWidget.java
@@ -2,21 +2,26 @@ package gripe._90.polyeng.widget;
 
 import appeng.client.gui.me.items.PatternEncodingTermScreen;
 import appeng.menu.me.items.PatternEncodingTermMenu;
+import appeng.menu.slot.PatternTermSlot;
 import appeng.parts.encoding.EncodingMode;
+import com.illusivesoulworks.polymorph.api.client.base.ITickingRecipesWidget;
 import com.illusivesoulworks.polymorph.client.recipe.widget.PlayerRecipesWidget;
 import com.mojang.blaze3d.vertex.PoseStack;
 import gripe._90.polyeng.PolymorphicEnergistics;
 import gripe._90.polyeng.mixin.AEBaseMenuAccessor;
-import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import gripe._90.polyeng.mixin.AbstractContainerScreenAccessor;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.inventory.Slot;
 
-public class PatternTerminalWidget extends PlayerRecipesWidget {
+public class PatternTerminalWidget extends PlayerRecipesWidget implements ITickingRecipesWidget {
     private final PatternEncodingTermMenu menu;
+    private Slot outputSlot;
+    private int menuHeight;
 
-    public PatternTerminalWidget(AbstractContainerScreen<?> containerScreen, Slot outputSlot) {
-        super(containerScreen, outputSlot);
-        this.menu = ((PatternEncodingTermScreen<?>) containerScreen).getMenu();
+    public PatternTerminalWidget(PatternEncodingTermScreen<?> screen, Slot outputSlot) {
+        super(screen, outputSlot);
+        this.outputSlot = outputSlot;
+        menu = screen.getMenu();
     }
 
     @Override
@@ -36,5 +41,27 @@ public class PatternTerminalWidget extends PlayerRecipesWidget {
     @Override
     public boolean mouseClicked(double mouseX, double mouseY, int button) {
         return menu.mode == EncodingMode.CRAFTING && super.mouseClicked(mouseX, mouseY, button);
+    }
+
+    @Override
+    public Slot getOutputSlot() {
+        return outputSlot;
+    }
+
+    @Override
+    public void tick() {
+        var screen = (AbstractContainerScreenAccessor) containerScreen;
+
+        if (screen.getImageHeight() != menuHeight) {
+            for (var slot : containerScreen.getMenu().slots) {
+                if (slot instanceof PatternTermSlot patternSlot) {
+                    outputSlot = patternSlot;
+                    resetWidgetOffsets();
+                    break;
+                }
+            }
+
+            menuHeight = screen.getImageHeight();
+        }
     }
 }
